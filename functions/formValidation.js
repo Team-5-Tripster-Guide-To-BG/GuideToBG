bcc5ebe7c4f80be7524b2b88c7994ad527704904
AlexPetrova <a.petrova95@yahoo.com>
2018-02-23 11:43:46 +0200

    \\

--- functions/formValidation.js
+++
@@ -1,9 +0,0 @@
-function validateName() {
-   var inputName = document.getElementById("name");
-   if(inputName.value == ""){
-     document.getElementById("validateMsg").innerHTML = "Name must be filled.";
-   }
-   else {
-     document.getElementById("validateMsg"). innerHTML = "";
-   }
-}
