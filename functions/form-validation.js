a368b38e3ee0bc32ecebf1c3abea882211c97d63
AlexPetrova <a.petrova95@yahoo.com>
2018-02-24 21:37:01 +0200

    Added alert messages
    still working on the css

--- functions/form-validation.js
+++ functions/form-validation.js
@@ -1,17 +1,47 @@
-function validateName() {
-  var para = document.createElement("span");
-  var node = document.createTextNode("This is new.");
-  para.appendChild(node);
+function validateForm() {
+  var isValid = true;
 
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var errorList = document.getElementById("error-msg");
 
-  var element = document.createElement("p");
-  var msg = document.createTextNode("Please, fill your name.");
-  element.appendChild(msg);
+  $("#error-msg li").remove();
 
-  var name = document.getElementById("name-field").appendChild(element);
+  var eventHandler = function(event) {
+    event.preventDefault();
+  };
 
-  if(name.value === ""){
-    name.appendChild(element);
+  var reMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  var reName = /^[A-Za-z]+$/;
+
+  if(name == "" || email == "")
+  {
+    var element = document.createElement("li");
+    var msg = document.createTextNode("Please enter name and email address.");
+    element.appendChild(msg);
+    element.className = "alert";
+    errorList.appendChild(element);
+    isValid = false;
   }
 
+  if(!reName.test(name) && name){
+    var element = document.createElement("li");
+    var msg = document.createTextNode("The name field can't contain any numbers or special characters.");
+    element.appendChild(msg);
+    element.className = "alert";
+    errorList.appendChild(element);
+    isValid = false;
+  }
+
+  if(!reMail.test(email) && email){
+    var element = document.createElement("li");
+    var msg = document.createTextNode("Please enter a valid email address.");
+    element.appendChild(msg);
+    element.className = "alert";
+    errorList.appendChild(element);
+    isValid = false;
+  }
+
+  return isValid;
 }
