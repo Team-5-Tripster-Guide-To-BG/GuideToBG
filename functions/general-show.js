62a9e4f8a649af9c9c232f00cc900d29c0aea759
Sophia Kiryakova <sophia.kiryakova@gmail.com>
2018-02-26 20:44:48 +0200

    Added Toggle button for General Info which changes its text when pushed.

--- functions/general-show.js
+++ functions/general-show.js
@@ -1,5 +1,14 @@
 (function () {
-
+    $("#show-general-info").click(function(){
+        $("#general-info-show-area").toggle();
+    });
+    $(function(){
+        $("#show-general-info").click(function () {
+            $(this).text(function(i, text){
+                return text === "Show More" ? "Show Less" : "Show More";
+            })
+        });
+    })
     $(document).on('click', '#show-general-info',  function() {
         $('#general-info-show-area').html(function () {
             const showGeneralInfoController = showGeneralInfo();
