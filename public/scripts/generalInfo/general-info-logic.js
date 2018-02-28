36ffb63b58ee19ab90ea77751336a200a4170c0a
Sophia Kiryakova <sophia.kiryakova@gmail.com>
2018-02-28 22:01:37 +0200

    Refactoring.

--- public/scripts/generalInfo/general-info-logic.js
+++ public/scripts/generalInfo/general-info-logic.js
@@ -1,7 +1,6 @@
 const generalInfoJSON = '{"generalInfo":"Bulgaria, officially the Republic of Bulgaria, is a country in southeastern Europe. It is bordered by Romania to the north, Serbia and Macedonia to the west, Greece and Turkey to the south, and the Black Sea to the east. Bulgaria is Europe\'s 16th-largest country."}';
-const strGeneralInfoJSON = JSON.parse(generalInfoJSON);
 
-document.getElementById("general-info-text").innerHTML = strGeneralInfoJSON.generalInfo;
+$("#general-info-text").html(JSON.parse(generalInfoJSON).generalInfo);
 
 $(".general-info-container").on("click", "#show-general-info", function () {
 
--- public/scripts/tripsters-info-logic.js
+++ public/scripts/tripsters-info-logic.js
@@ -1,4 +1,3 @@
 const tripstersInfoJSON = '{"tripstersInfo":"Tripster: Tripster is a travel hipster. Someone who is so over the mainstream tourist destinations and they only prefer to go to the bleakest, most obsucure regions of the planet. That being said we will be your tripster guides to discover one of the most obscure places of the planet - our home, Bulgaria."}';
-const strTripstersInfoJSON = JSON.parse(tripstersInfoJSON);
 
-document.getElementById("tripsters-info-text").innerHTML = strTripstersInfoJSON.tripstersInfo;
+$("#tripsters-info-text").html(JSON.parse(tripstersInfoJSON).tripstersInfo);
